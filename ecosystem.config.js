0c9bd7ddaa2070d06d09dd41414979a8c63e97e1
RiderKick <63233474+ridershoot@users.noreply.github.com>
2024-06-19 15:35:36 +0800

    chore: 调整打包配置

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   apps: [
     {
-      name: "nuxt3-template", // 设置启动项目名称
-      port: "3001", // 监听端口
+      name: "nuxt3-template",
+      port: "3000",
       exec_mode: "cluster",
+      // 实例数量
       instances: "max",
-      // 注意这里的相对路径。要访问到index.mjs就行了，如果你是整个.output一起放在服务器的话就和官方一样路写成./.output/server/index.mjs就好了
       script: "./.output/server/index.mjs",
     },
   ],
--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,7 @@
 
 <script lang="ts" setup>
 const config = useRuntimeConfig();
+console.log(config.public);
 </script>
 
 <style lang="scss" scoped></style>
